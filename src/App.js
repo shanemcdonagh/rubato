488a862adb8ac48f0895ddb1275f1902bc8e229d
SHANE MC DONAGH - STUDENT <g00371430@atu.ie>
2022-11-18 14:28:48 +0000

    Created basic files for different pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+// Local Components to import
+
+// Allows for dynamic routing and switching between components
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // App class - extends Component class
 class App extends Component {
   // Visual content - Method
-  render()
-  {  
-    return(
-      <div className="App">
-      <header className="App-header">
-        <img src='./simplelogo.png' alt='rubitologo'/>
-        <p>
-          Rubito
-        </p>
-        <a>Your taste, your music</a>
-      </header>
-    </div>
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src='./simplelogo.png' alt='rubitologo' />
+            <p>
+              Rubito
+            </p>
+            <a>Your taste, your music</a>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Home extends Component {
+    render() {
+            <p>This is the Home Page (where certain artists and albums will be displayed)</p>
+    }
+}
+
+// Export for use in App.js
+export default Home;
