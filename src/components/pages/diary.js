a1359c6b198b59f30c345e158fd46983d623c496
Shane McDonagh <G00371430@gmit.ie>
2022-11-21 12:44:16 +0000

    Added searchbar feature, issues occur with page not fitting fully to screen with this feature

--- src/components/pages/diary.js
+++ src/components/pages/diary.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 class Diary extends Component {
     render() {
--- src/components/pages/lists.js
+++ src/components/pages/lists.js
@@ -1,4 +1,5 @@
-import { Component } from "react";
+import React, { Component } from "react";
+
 
 class Lists extends Component {
     render() {
