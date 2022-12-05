7c53fa4cb8a2d07300aa12b2a7cf0c1cb32ba44b
SHANE MC DONAGH - STUDENT <g00371430@atu.ie>
2022-12-05 14:54:32 +0000

    Established connection to MongoDB in the backend

--- backend/backend.js
+++ backend/backend.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Specify port for server to run on
 const port = 4000;
@@ -13,6 +14,10 @@ app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()) // parse application/json
+
+// Specifies what the server can parse
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json())
 
 // Allows for requests and responses to be made over different domains
 app.use(cors());
@@ -26,6 +31,17 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Used to connect to our MongoDB database
+const connectionString = "mongodb+srv://admin:admin@cluster0.cehtzag.mongodb.net/?retryWrites=true&w=majority"
+
+// Asynchronous function
+async function main() {
+    // Connect to the database
+    mongoose.connect(connectionString, { useNewUrlParser: true });
+}
+
+// Log an error if one occurs when connecting to the database
+main().catch(err => console.log(err));
 
 // Server begins listening through port 4000, handles requests from port 3000 (our music application)
 app.listen(port, (req, res) => {
