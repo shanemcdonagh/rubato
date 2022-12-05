46f39e2822e86cc03d4e35e9922088bc595abd75
SHANE MC DONAGH - STUDENT <g00371430@atu.ie>
2022-12-05 12:54:01 +0000

    Created backend server template, added dependencies

---
+++ backend/backend.js
@@ -0,0 +1,33 @@
+// Server Dependencies
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Specify port for server to run on
+const port = 4000;
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
+app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()) // parse application/json
+
+// Allows for requests and responses to be made over different domains
+app.use(cors());
+
+// Specifies the HTTP methods in which can be used from different domains and from where
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+// Server begins listening through port 4000, handles requests from port 3000 (our music application)
+app.listen(port, (req, res) => {
+    console.log(`Listening at http://localhost:${port}`);
+});
