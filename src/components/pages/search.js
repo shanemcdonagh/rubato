565cedb51342ce62d9e874f25222516ecd2535ef
Shane McDonagh <G00371430@gmit.ie>
2022-12-15 17:20:26 +0000

    Made card template for metadata to use for visual effect

--- src/components/pages/search.js
+++ src/components/pages/search.js
@@ -1,26 +1,55 @@
 import React, { Component } from "react";
-import { Navbar, FormControl, Card, Row, Container, InputGroup } from 'react-bootstrap';
+import { FormControl, Card, Row, Container, InputGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
 
 class Search extends Component {
+    // https://reactjs.org/docs/hooks-state.html
+    // Initially ensures search value is empty
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: ""
+        };
+    }
+
     render() {
-        return (<div className="content">
-                <InputGroup className="mb-3" size="lg">
+        return (<div>
+            <InputGroup className="mb-3" size="lg" style={{ marginTop: '5vh', marginLeft: '50vh' }}>
                 <FormControl
-                  style={{ marginLeft: "45%" }}
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                  onKeyDown={event => {
-                    console.log("Hey")
-                  }}
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    onKeyDown={event => {
+                        console.log("Hey")
+                    }}
+                    // When the value of the form changes, update the value of the search field
+                    onChange={event => this.setState({ search: event.target.value })}
                 />
-                <Button variant="outline-danger">Search</Button>
-                </InputGroup>
-                
-            <p>This is the Search Page (where users can retrieve artists and albums)</p></div>
+                <Button variant="outline-danger" onClick={() => { console.log("Button click") }}>Search</Button>
+            </InputGroup>
+            <div className="content">
+                <Container>
+                    <Row className="mx-2 row row-col-4">
+                        <Card
+                            bg="dark"
+                            text="danger"
+                            style={{ width: '18rem' }}
+                            className="mb-2">
+                            <Card.Header>Artist</Card.Header>
+                            <Card.Body>
+                                <Card.Title></Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                </Container>
+            </div>
+        </div>
         );
     }
 }
