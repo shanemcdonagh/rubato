066fa8728cab4ba37f3666c8c81a1a2928011237
SHANE MC DONAGH - STUDENT <g00371430@atu.ie>
2023-01-20 11:22:51 +0000

    Began working on the profile page

--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import Image from "react-bootstrap/Image";
 
 class Profile extends Component {
     render() {
-        return (<div className="content"><p>This is the Profile Page (where user information will be displayed)</p></div>
+        return (
+        <div className="content">
+            <p>This is the Profile Page (where user information will be displayed)</p>
+            <div>
+            <Image src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" width="150px" roundedCircle/>                
+            {/* https://mdbcdn.b-cdn.net/img/new/avatars/8.webp */}
+            </div>
+        </div>
         );
     }
 }
