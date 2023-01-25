11698f2fdbaa144b47a897caf52f4b18f589dfe6
SHANE MC DONAGH - STUDENT <g00371430@atu.ie>
2023-01-25 15:10:02 +0000

    Fixed how list page handled state, initially was not efficient

--- src/components/pages/lists.js
+++ src/components/pages/lists.js
@@ -16,13 +16,20 @@ class Lists extends Component {
         this.state = {
             setShow: false
         };
+
+        // Binding this keyword
+        this.handleClick = this.handleClick.bind(this)
     }
-    
+
+    handleClick() {
+        // Changing state
+        this.setState({setShow : true})
+    }
 
     render() {
         return (<div className="content">
             <p>This is the Lists Page (where albums you want to listen later and organise are)</p>
-            <Button variant="danger" onClick={this.setState({setShow: true})}>Create a list</Button>            
+            <Button variant="danger" onClick={this.handleClick}>Create a list</Button>
             </div>
         );
     }
