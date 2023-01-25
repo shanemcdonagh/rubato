303351ac68bf4322af7a9427ab6e8f96a7d50e10
SHANE MC DONAGH - STUDENT <g00371430@atu.ie>
2023-01-25 15:00:05 +0000

    Began adding modal view for adding lists

--- src/components/pages/lists.js
+++ src/components/pages/lists.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
 
-
+// Lists will showcase all lists that have been created by a given user
 class Lists extends Component {
+
+    // Reference: https://react-bootstrap.github.io/components/modal/
+    // Allows us to add a new list to our lists page
+    // [show, setShow]  useState(false);
+    // handleClose = () => setShow(false);
+    // handleShow = () => setShow(true);
+     // https://reactjs.org/docs/hooks-state.html
+    // Initially ensures search value and card values are empty
+    constructor(props) {
+        super(props);
+        this.state = {
+            setShow: false
+        };
+    }
+    
+
     render() {
-        return (<div className="content"><p>This is the Lists Page (where albums you want to listen later and organise are)</p></div>
+        return (<div className="content">
+            <p>This is the Lists Page (where albums you want to listen later and organise are)</p>
+            <Button variant="danger" onClick={this.setState({setShow: true})}>Create a list</Button>            
+            </div>
         );
     }
 }
